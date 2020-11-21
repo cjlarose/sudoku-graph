70511ba0a8fa8e5abcf744982f331c2afd5e71d7
Chris LaRose <cjlarose@gmail.com>
2020-11-20 23:00:01 -0800

    Add allColors function

--- src/Sudoku/VertexColor.purs
+++ src/Sudoku/VertexColor.purs
@@ -1,5 +1,14 @@
 module Sudoku.VertexColor
   ( VertexColor
+  , allColors
   ) where
 
+import Prelude
+
+import Data.Set as Set
+import Data.Array ((..))
+
 type VertexColor = Int
+
+allColors :: Set.Set VertexColor
+allColors = Set.fromFoldable $ 0 .. 8
