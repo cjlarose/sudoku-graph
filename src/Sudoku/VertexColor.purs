debbedce9bb4b30efef78bc9f6c2a43bb68d84d8
Chris LaRose <cjlarose@gmail.com>
2020-11-21 19:04:22 -0800

    Wrap VertexColor show output in parens

--- src/Sudoku/VertexColor.purs
+++ src/Sudoku/VertexColor.purs
@@ -14,7 +14,7 @@ derive instance eqVertexColor :: Eq VertexColor
 derive instance ordVertexColor :: Ord VertexColor
 
 instance showVertexColor :: Show VertexColor where
-  show (VertexColor color) = "VertexColor " <> show color
+  show (VertexColor color) = "(VertexColor " <> show color <> ")"
 
 allColors :: Set.Set VertexColor
 allColors = Set.map VertexColor <<< Set.fromFoldable $ 0 .. 8
