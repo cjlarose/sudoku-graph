467fb4888f3f201bb319fe18dcc2156c20a5a6d4
Chris LaRose <cjlarose@gmail.com>
2020-12-02 23:57:48 -0800

    Move puzzle parser into new module

---
+++ src/Sudoku/Parse.purs
@@ -0,0 +1,43 @@
+module Sudoku.Parse
+  ( problem
+  , problems
+  ) where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Data.Char (toCharCode)
+import Control.Alt ((<|>))
+import Text.Parsing.Parser (ParserT)
+import Text.Parsing.Parser.Combinators (sepEndBy1)
+import Text.Parsing.Parser.String (char, oneOf, eof)
+import Data.Array (some, zipWith)
+import Data.List (List)
+import Data.Map as Map
+import Data.Set as Set
+import Data.Foldable (fold)
+
+import Sudoku.PartialColoring (PartialColoring(..), Coord, allCoords)
+import Sudoku.VertexColor (VertexColor, fromInt)
+
+emptyCell :: forall m. Monad m => ParserT String m (Maybe VertexColor)
+emptyCell = Nothing <$ char '.'
+
+cellWithClue :: forall m. Monad m => ParserT String m (Maybe VertexColor)
+cellWithClue = fromInt <<< (\c -> toCharCode c - toCharCode '0') <$> oneOf ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+cellContent :: forall m. Monad m => ParserT String m (Maybe VertexColor)
+cellContent = emptyCell <|> cellWithClue
+
+problem :: forall m. Monad m => ParserT String m PartialColoring
+problem = toPartialColoring <$> some cellContent
+  where
+    toColoring :: Coord -> Maybe VertexColor -> Map.Map Coord VertexColor
+    toColoring _ Nothing = Map.empty
+    toColoring coord (Just color) = Map.insert coord color Map.empty
+
+    toPartialColoring :: Array (Maybe VertexColor) -> PartialColoring
+    toPartialColoring xs = PartialColoring <<< fold $ zipWith (\coord content -> toColoring coord content) (Set.toUnfoldable allCoords) xs
+
+problems :: forall m. Monad m => ParserT String m (List PartialColoring)
+problems = sepEndBy1 problem (char '\n') <* eof
--- test/Sudoku/MagicTour.purs
+++ test/Sudoku/MagicTour.purs
@@ -9,50 +9,20 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Node.FS.Aff as FS
 import Node.Encoding (Encoding(..))
-import Data.Maybe (Maybe(..))
-import Data.Char (toCharCode)
 import Data.Either (Either(..))
-import Control.Alt ((<|>))
-import Text.Parsing.Parser (ParserT, runParser)
-import Text.Parsing.Parser.Combinators (sepEndBy1)
-import Text.Parsing.Parser.String (char, oneOf, eof)
-import Data.Array (some, zipWith)
+import Text.Parsing.Parser (runParser)
 import Data.List (List, mapWithIndex)
-import Data.Map as Map
-import Data.Set as Set
 import Data.Tuple (Tuple(..))
-import Data.Foldable (fold, for_)
+import Data.Foldable (for_)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Main (runTestWith)
 import Test.Unit.Output.Simple as SimpleOutput
 import Test.Unit.Assert as Assert
 
-import Sudoku.PartialColoring (PartialColoring(..), Coord, allCoords)
-import Sudoku.VertexColor (VertexColor, fromInt)
+import Sudoku.PartialColoring (PartialColoring)
 import Sudoku.Worksheet (Worksheet(..), completeWithAnnotations)
 import Sudoku.Solve (solveWorksheet)
-
-emptyCell :: forall m. Monad m => ParserT String m (Maybe VertexColor)
-emptyCell = Nothing <$ char '.'
-
-cellWithClue :: forall m. Monad m => ParserT String m (Maybe VertexColor)
-cellWithClue = fromInt <<< (\c -> toCharCode c - toCharCode '0') <$> oneOf ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-cellContent :: forall m. Monad m => ParserT String m (Maybe VertexColor)
-cellContent = emptyCell <|> cellWithClue
-
-problem :: forall m. Monad m => ParserT String m PartialColoring
-problem = toPartialColoring <$> some cellContent
-  where
-    toColoring :: Coord -> Maybe VertexColor -> Map.Map Coord VertexColor
-    toColoring _ Nothing = Map.empty
-    toColoring coord (Just color) = Map.insert coord color Map.empty
-
-    toPartialColoring :: Array (Maybe VertexColor) -> PartialColoring
-    toPartialColoring xs = PartialColoring <<< fold $ zipWith (\coord content -> toColoring coord content) (Set.toUnfoldable allCoords) xs
-
-problems :: forall m. Monad m => ParserT String m (List PartialColoring)
-problems = sepEndBy1 problem (char '\n') <* eof
+import Sudoku.Parse (problems)
 
 testMagicTour1465 :: List PartialColoring -> TestSuite
 testMagicTour1465 colorings = suite "magic tour" do
